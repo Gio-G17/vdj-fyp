6957beaf37debce58dc8275290cc27c8364b9d5e
Gio-G17 <giorgioghantous@gmail.com>
2025-04-28 20:58:26 +0300

    deleted dj db since no more use

--- backend/src/models/DJ.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DJSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  genre: { type: String, required: true },
-  image: { type: String, required: true }, // Store image URL
-});
-
-module.exports = mongoose.model("DJ", DJSchema);
